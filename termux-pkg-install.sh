3c78775d55c650ec6b63e98dbd88e0b59fada26a
Dmitry Shashkov (Brookit) <brookit18@gmail.com>
2025-12-11 23:50:41 +0300

    Return nodejs-lts in termux

--- termux-pkg-install.sh
+++ termux-pkg-install.sh
@@ -3,7 +3,7 @@ termux-setup-storage
 pkg upgrade
 pkg install tur-repo termux-api -y
 pkg install ncurses-utils curl wget git proot-distro openssl gnupg fakeroot mc openssh lz4 zstd 7zip rsync rclone neovim -y
-pkg install -y python rust nodejs openjdk-25 deno golang -y
+pkg install -y python rust nodejs-lts openjdk-25 deno golang -y
 pkg install -y libzmq libxml2 libxslt -y build-essential cmake fzf binutils binutils-gold dnsutils net-tools inetutils llvmgold termux-gui-package traceroute ninja -y
 python -m pip install --upgrade setuptools wheel
 pkg install python-numpy python-pillow python-pandas python-lxml python-scipy python-yt-dlp matplotlib ruff uv leveldb -y
